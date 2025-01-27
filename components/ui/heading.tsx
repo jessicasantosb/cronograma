72a02a370448a3b054e90f934bb009b611de822e
Jessica <jessica.santosb@outlook.com>
2025-01-27 19:56:49 -0300

    style: change title color

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -6,7 +6,9 @@ interface HeadingProps {
 export function Heading({ title, description }: HeadingProps) {
   return (
     <div>
-      <h2 className='text-3xl font-bold tracking-tight'>{title}</h2>
+      <h2 className='text-3xl font-bold tracking-tight text-blue-600'>
+        {title}
+      </h2>
       <p className='text-sm text-muted-foreground'>{description}</p>
     </div>
   );
